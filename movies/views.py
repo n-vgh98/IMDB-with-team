4ebe854d159492a6487c3cdd3fb66d9c756e3437
n-vgh98 <nazi.gh988@gmail.com>
2022-11-13 16:53:32 +0330

    create view and url and index template for movies list

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+
+def movies_list(request):
+    movies = Movie.objects.filter(is_valid=True)
+    return render(request, 'movies/index.html', {'movies': movies})
