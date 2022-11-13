363967137e399cf0d4cca855dde95d03e022624e
n-vgh98 <nazi.gh988@gmail.com>
2022-11-13 12:32:50 +0330

    create project and movies app, customize my project

---
+++ movies/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+
+class Genre(models.Model):
+    title = models.CharField(max_length=50)
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Role(models.Model):
+    title = models.CharField(max_length=50)
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Crew(models.Model):
+    MALE = 1
+    FEMALE = 2
+    GENDER_CHOICES = (
+        (MALE, ' male'),
+        (FEMALE, 'female')
+    )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birthday = models.DateField(null=True, blank=True)
+    gender = models.PositiveSmallIntegerField(choices=GENDER_CHOICES, default=MALE)
+    avatar = models.ImageField(upload_to='crew/avatars/', null=True, blank=True)
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.first_name} : {self.last_name}'
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    release_date = models.DateField(null=True, blank=True)
+    avatar = models.ImageField(upload_to='movies/avatars/', null=True, blank=True)
+    genres = models.ManyToManyField(Genre)
+    crew = models.ManyToManyField(Crew, through='MovieCrew')
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class MovieCrew(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    crew = models.ForeignKey(Crew, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('movie', 'crew', 'role')
