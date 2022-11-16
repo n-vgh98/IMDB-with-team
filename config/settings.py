5037b4977b700ebfa1eac54b480198d2b119907f
n-vgh98 <nazi.gh988@gmail.com>
2022-11-16 13:46:16 +0330

    add accounts app for login system,done login and logout

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movies.apps.MoviesConfig',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,6 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'movies_list'
+LOGOUT_REDIRECT_URL = 'movies_list'
