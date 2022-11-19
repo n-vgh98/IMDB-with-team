0f867998a986eb2e7bde78eb4efa7905816b86a7
n-vgh98 <nazi.gh988@gmail.com>
2022-11-19 16:43:56 +0330

    add UserAdmin for custom admin panel for users

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(User)
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('is_active', 'is_staff')
+
+
+admin.site.register(User, UserAdmin)
